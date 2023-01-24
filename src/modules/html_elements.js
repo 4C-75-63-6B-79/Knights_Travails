d81ee6a00c5092485dadda50930a128d69ca6cd7
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-24 11:57:17 +0530

    added make footer and main function in html element

--- src/modules/html_elements.js
+++ src/modules/html_elements.js
@@ -5,8 +5,22 @@ function makeHeader() {
     body.appendChild(header);
 }
 
-function start() {
-    makeHeader();
+function makeMain() {
+    const body = document.querySelector('body');
+    const main = document.createElement('main');
+    body.appendChild(main);
 }
 
-export  {start }; 
+function makeFooter() {
+    const body = document.querySelector('body');
+    const footer = document.createElement('footer');
+    body.appendChild(footer);
+}
+
+function start() {
+    makeHeader();
+    makeMain();
+    makeFooter();
+}
+
+export  { start }; 
