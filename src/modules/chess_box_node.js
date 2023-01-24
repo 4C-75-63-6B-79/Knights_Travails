b5e715fa8c36cf4c0b34bb6b5d84d3e22e796415
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-24 13:31:50 +0530

    added equals function in prototype of chess box node

--- src/modules/chess_box_node.js
+++ src/modules/chess_box_node.js
@@ -5,4 +5,12 @@ export default function ChessBoxNode(x, y, dist, parent) {
     this.parent = parent;
 }
 
-
+ChessBoxNode.prototype.equals = function(otherChessBoxNode) {
+    if(!ChessBoxNode.prototype.isPrototypeOf(otherChessBoxNode)) {
+        return false;
+    }
+    if(!(this.x === otherChessBoxNode.x && this.y === otherChessBoxNode)) {
+        return false;
+    }
+    return true;
+}
